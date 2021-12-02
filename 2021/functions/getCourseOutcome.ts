b9262bd964c11641ae291f756fc136b6b26e6a64
Marvin Zeising <marvin.zeising@gmail.com>
2021-12-02 12:27:35 +0100

    fourth puzzle

--- 2021/functions/getCourseOutcome.ts
+++ 2021/functions/getCourseOutcome.ts
@@ -1,3 +1,42 @@
+type Outcome = {
+  depth: number;
+  position: number;
+  aim: number;
+};
+
+export function getCourseOutcome(instructions: string[]): Outcome {
+  return instructions.reduce(
+    (total: Outcome, instruction) => {
+      const [direction, amount] = instruction.split(" ");
+      if (direction === "down") {
+        return {
+          ...total,
+          aim: total.aim + parseInt(amount),
+        };
+      }
+      if (direction === "up") {
+        return {
+          ...total,
+          aim: total.aim - parseInt(amount),
+        };
+      }
+      if (direction === "forward") {
+        return {
+          ...total,
+          position: total.position + parseInt(amount),
+          depth: total.depth + total.aim * parseInt(amount),
+        };
+      }
+      return total;
+    },
+    {
+      depth: 0,
+      position: 0,
+      aim: 0,
+    }
+  );
+}
+
 export function getCourseOutcomeDepth(instructions: string[]): number {
   return instructions.reduce((total, instruction) => {
     const [direction, amount] = instruction.split(" ");
--- 2021/index.ts
+++ 2021/index.ts
@@ -1,15 +1,7 @@
-import { getCourseOutcomeDepth } from "./functions/getCourseOutcome.ts";
+import { getCourseOutcome } from "./functions/getCourseOutcome.ts";
 import instructions from "./inputs/instructions.ts";
-import { getCourseOutcomePosition } from "./functions/getCourseOutcome.ts";
 
-// const windowSums = toThreeMeasurementWindows(exampleDepths).map((ds) =>
-//   ds.reduce((a, b) => a + b)
-// );
-
-// const result = getNumberOfIncreases(windowSums);
-
-const depth = getCourseOutcomeDepth(instructions);
-const position = getCourseOutcomePosition(instructions);
+const { depth, position } = getCourseOutcome(instructions);
 
 console.log("depth: ", depth, " position: ", position);
 console.log(depth * position);
